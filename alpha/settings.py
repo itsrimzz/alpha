8bf8c9d4eaf15041187f8b2a2828f4869e1af3fb
itsrimzz1 <rishabh.g@hashedin.com>
2017-03-26 19:37:24 +0530

    resolved static_root

--- alpha/settings.py
+++ alpha/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = ""
+STATIC_ROOT = BASE_DIR
 STATICFILES_DIRS = ( os.path.join('static'), )
 LOGIN_REDIRECT_URL = 'homepage'
 LOGOUT_REDIRECT_URL = 'homepage'
